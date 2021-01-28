bee0b8b2890d44ed410dadecd82ae0a4981c7c05
Artem Vdovin <artem.vdovin@mail.ru>
2021-01-28 21:32:39 +0300

    Добавление полноэкранного режима

--- app/src/main/java/com/example/gribyandrasteniyamap/MainActivity.java
+++ app/src/main/java/com/example/gribyandrasteniyamap/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.gribyandrasteniyamap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullScreen();
         setContentView(R.layout.activity_main);
     }
+
+    private void setFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+
 }
